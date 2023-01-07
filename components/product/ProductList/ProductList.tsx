8ce5a39d6006d87cd9461910c1f1fa4af153e8cd
musyafa619 <musyafaarts@gmail.com>
2023-01-08 03:44:29 +0700

    ✨ product list bottom sheet

--- components/product/ProductList/ProductList.tsx
+++ components/product/ProductList/ProductList.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Grid, Divider } from '@mui/material';
+import { useCart } from 'context/CartContext';
 import { ProductDto } from 'libs/dto/products';
 import ProductCard from '../ProductCard';
 
@@ -7,8 +8,9 @@ interface Props {
 }
 
 const ProductList: React.FC<Props> = ({ products }) => {
+  const { items } = useCart();
   return (
-    <Box sx={{ width: '100%', pb: 5 }}>
+    <Box sx={{ width: '100%', pb: items.length < 1 ? 5 : 15 }}>
       <Typography variant="h5" fontWeight={600} sx={{ mb: 5 }}>
         Products
       </Typography>
